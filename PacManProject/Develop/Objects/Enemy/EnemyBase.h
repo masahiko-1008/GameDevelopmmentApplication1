102c5cc423cf49f85aef749dd2adc8045ede60c0
masahiko-1008 <138412620+masahiko-1008@users.noreply.github.com>
2024-08-27 09:34:43 +0900

    パックマン課題

--- PacManProject/Develop/Objects/Enemy/EnemyBase.h
+++ PacManProject/Develop/Objects/Enemy/EnemyBase.h
@@ -2,8 +2,32 @@
 
 #include "../GameObject.h"
 
+enum eDirectionState
+{
+	MOVE,
+	IJIKE,
+	HOME
+};
+
 class EnemyBase : public GameObject
 {
+	enum EnemyState
+	{
+		PURSUE,
+		AMBUSH,
+		REFLECTION,
+		RANDAM,
+		TERRITORY,
+		WAIT
+	};
+
+private:
+	std::vector<int> move_animation;
+	std::vector<int> dying_animation;
+	Vector2D old_location;
+	Vector2D veloctity;
+	eEnemyState : eEnemyState
+
 protected:
 
 public:
